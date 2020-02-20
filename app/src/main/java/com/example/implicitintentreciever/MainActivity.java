18d3487b150af52f0ef44e728f8d3097afecc0b6
Luke Gaudiano <lukegaudiano@yahoo.com>
2020-02-20 11:22:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/implicitintentreciever/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.implicitintentreciever;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.implicitintentreciever.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uri_string = getString(R.string.uri_label) + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
+    }
+}
